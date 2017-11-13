14fb1ab7923ea3a1304d6dc09ade8d38033c76ce
Nick Bielawski <nbielawski12@gmail.com>
2017-11-13 16:46:49 -0500

    first commit

---
+++ FizzBuzzMvc/Controllers/FizzBuzzController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzBuzzMvc.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+
+        private string ToFizzBuzz(int value)
+        {
+            var isDivisibleByFive = value % 5 == 0;
+            var isDivisibleByThree = value % 3 == 0;
+
+            if (isDivisibleByFive && isDivisibleByThree) return "FizzBuzz";
+            if (isDivisibleByFive) return "Buzz";
+            if (isDivisibleByThree) return "Fizz";
+
+            return $"{value}";
+        }
+
+        // GET: FizzBuzz
+        public ActionResult Index()
+        {
+
+            var model = Enumerable.Range(1, 100).Select(ToFizzBuzz);
+            return View(model);
+        }
+    }
+}
